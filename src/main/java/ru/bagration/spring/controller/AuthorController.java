1b8414f28b161c0b3d1d456a17ccd235c09e4245
Pochapil <bagrat37@mail.ru>
2022-04-04 14:19:22 +0300

    change in controller

--- src/main/java/ru/bagration/spring/controller/AuthorController.java
+++ src/main/java/ru/bagration/spring/controller/AuthorController.java
@@ -1,52 +1,42 @@
 package ru.bagration.spring.controller;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.bagration.spring.dto.AuthorDtoAddValid;
 import ru.bagration.spring.dto.CreateAuthorDto;
 import ru.bagration.spring.service.AuthorService;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/author")
 @RequiredArgsConstructor
+@RequestMapping("/author")
 public class AuthorController {
 
     private final AuthorService authorService;
 
     @PostMapping
-    ResponseEntity<?> createAuthor(@Valid @RequestBody CreateAuthorDto dto) {
-        System.out.println("-------------request body--------------");
-        System.out.println(dto);
-        System.out.println("----------------------------------------");
-        return authorService.createAuthor(dto.getFirstName(), dto.getSecondName());
+    ResponseEntity<?> createAuthor(@Valid @RequestBody CreateAuthorDto dto){
+        return authorService.createAuthor(dto.getFirstName(), dto.getLastName());
     }
 
     @GetMapping("/{id}")
-    ResponseEntity<?> getDetailed(@PathVariable String id){
+    ResponseEntity<?> getDetails(@PathVariable String id){
         return authorService.getDetailsById(id);
     }
 
-
     @GetMapping("/list")
-    ResponseEntity<?> authorsList(@RequestParam(name = "name", defaultValue = "", required = false) String name,
-                                  @RequestParam(name = "firstName", required = false) String firstName,
-                                  @RequestParam(name = "secondName", required = false) String secondName,
-                                  @RequestParam(name = "detailed", required = false, defaultValue = "false") Boolean detailed,
-                                  @PageableDefault(size = 25, page = 0,
-                                          sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable) {
-        return authorService.getAuthorList(name, firstName, secondName, detailed, pageable);
-    }
-
-    @PostMapping("/add")
-    ResponseEntity<?> addNewAuthor(@RequestBody @Valid AuthorDtoAddValid dto){
-        return authorService.addNewAuthor(dto.getFirstName(), dto.getSecondName());
+    ResponseEntity<?> authorsList(@RequestParam(name = "name", defaultValue = "", required = false)String name,
+                                  @RequestParam(name = "firstName", required = false)String firstName,
+                                  @RequestParam(name = "lastName", required = false)String lastName,
+                                  @RequestParam(name = "detailed", required = false, defaultValue = "false")
+                                          Boolean detailed,
+                                  @PageableDefault(size = 25, sort = {"id"}, direction = Sort.Direction.DESC)
+                                          Pageable pageable){
+        return authorService.getAuthorsList(name, firstName, lastName, detailed, pageable);
     }
 
 }
--- src/main/java/ru/bagration/spring/controller/PublicationController.java
+++ src/main/java/ru/bagration/spring/controller/PublicationController.java
@@ -1,68 +1,39 @@
 package ru.bagration.spring.controller;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.bagration.spring.dto.CreatePublicationDto;
 import ru.bagration.spring.dto.NewPublicationDto;
-import ru.bagration.spring.dto.PublicationDto;
-import ru.bagration.spring.dto.PublicationDtoAddValid;
 import ru.bagration.spring.service.PublicationService;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/publication")
 @RequiredArgsConstructor
+@RequestMapping("/publication")
 public class PublicationController {
 
     private final PublicationService publicationService;
 
-    @GetMapping("/feed")
-    public ResponseEntity<?> getFeed() {
-        return ResponseEntity.ok(publicationService.getAllPublications());
-    }
-    
-    @PostMapping
-    ResponseEntity<?> createPublication(@Valid @RequestBody CreatePublicationDto dto) {
-        System.out.println("-------------request body--------------");
-        System.out.println(dto);
-        System.out.println("----------------------------------------");
-        return publicationService.createPublication(dto.getAuthorId(), dto.getTitle(), dto.getThemeId(), dto.getContent());
-    }
-
     @GetMapping("/list")
-    ResponseEntity<?> publicationList(@RequestParam(name = "searchString", defaultValue = "", required = false) String searchString,
-                                      @RequestParam(name = "title", required = false) String title,
-                                      @RequestParam(name = "content", required = false) String content,
-                                      @RequestParam(name = "detailed", required = false, defaultValue = "false") Boolean detailed,
-                                      @PageableDefault(size = 25, page = 0,
-                                              sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable) {
-        return publicationService.getPublicationList(searchString, title, content, detailed, pageable);
-    }
-
-
-    @GetMapping("/list/rh")
-    ResponseEntity<?> getData(@RequestParam(name = "author_name", defaultValue = "", required = false) String authorName,
-                              @RequestParam(name = "theme_name", defaultValue = "", required = false) String themeName,
-                              @PageableDefault(size = 25, page = 0,
-                                      sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable) {
+    ResponseEntity<?> getData(@RequestParam(name = "author_name", required = false, defaultValue = "")String authorName,
+                              @RequestParam(name = "theme_name", required = false, defaultValue = "")String themeName,
+                              Pageable pageable){
         return publicationService.getData(authorName, themeName, pageable);
     }
 
-
-    @GetMapping("/{id}")
-    ResponseEntity<?> getDetailed(@PathVariable String id) {
-        return publicationService.getDetailsById(id);
+    @GetMapping("/feed")
+    public ResponseEntity<?> getFeed(){
+        return publicationService.getAllPublications();
     }
 
-    @PostMapping("/add")
-    ResponseEntity<?> addNewPublication(@RequestBody @Valid NewPublicationDto dto){
-        return publicationService.addNewPublication(dto.getAuthorId(), dto.getThemeId(), dto.getTitle(), dto.getContent());
+    @PostMapping
+    public ResponseEntity<?> addNewPublication(@Valid @RequestBody NewPublicationDto dto){
+        return publicationService.createPublication(dto.getAuthorId(), dto.getThemeId(),
+                dto.getTitle(), dto.getContent());
     }
+
+
 
 }
